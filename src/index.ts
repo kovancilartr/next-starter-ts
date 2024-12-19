9005f15c9272c69abf1ee7d09bd74e0128e85765
Yalçın <kovancilartr@gmail.com>
2024-12-20 01:03:40 +0300

    2

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import express, { Request, Response } from "express";
+import express from "express";
+import customerRouter from "./routes/customer";
 
 require("dotenv").config();
 const cors = require("cors");
@@ -14,19 +15,4 @@ app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
 
-// Create an API
-// GET, POST, PUT, PATCH, DELETE
-
-// http:localhost:3001/customers
-app.get("/customers", async (req: Request, res: Response) => {
-  const customers = [
-    { name: "John Doe", email: "john.doe@example.com", phone: "+1234567890" },
-    {
-      name: "Joel Smith",
-      email: "joel.smith@example.com",
-      phone: "+0987654321",
-    },
-  ];
-
-  return res.status(200).json(customers);
-});
+app.use("/api/v1", customerRouter);
---
+++ src/routes/customer.ts
@@ -0,0 +1,13 @@
+import {
+  createCustomer,
+  getCustomerById,
+  getCustomers,
+} from "@/controllers/customers";
+import express from "express";
+const customerRouter = express.Router();
+
+customerRouter.post("/customers", createCustomer);
+customerRouter.get("/customers", getCustomers);
+customerRouter.get("/customers/:id", getCustomerById);
+
+export default customerRouter;
